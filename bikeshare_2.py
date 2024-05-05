711db4c9db799a544bf299a7c369f886f8f64c9c
Vinicius Sá <viniciusagsa@gmail.com>
2024-05-05 15:20:19 -0300

    docs: update time_stats docstring

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -122,7 +122,14 @@ def load_data(city, month, day):
 
 
 def time_stats(df):
-    """Displays statistics on the most frequent times of travel."""
+    """Displays statistics on the most frequent times of travel.
+
+    Args:
+        (dataframe) df - Dataframe with 'Start Time' column.
+    Returns:
+        print time stats statistcs
+    
+    """
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
